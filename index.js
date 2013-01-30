77e48e7d48b43db1232bfba5d6999db52a910b46
Tobias Koppers <tobias.koppers@googlemail.com>
2013-01-30 18:02:15 +0100

    fix for webpack 0.9

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function(minetype, postfix, prefix) {
-	var fileLoader = require("file-loader")(postfix, prefix);
-	return function(content) {
-		this.cacheable && this.cacheable();
-		var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
-		var buf;
-		if(this.buffers) buf = this.buffers[0];
-		else buf = new Buffer(content, "utf-8");
-		if(buf.length < limit) {
-			return "module.exports = " + JSON.stringify("data:" + (minetype ? minetype + ";" : "") + "base64," + buf.toString("base64"));
-		} else {
-			return fileLoader.call(this, content);
-		}
+var loaderUtils = require("loader-utils");
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	var query = loaderUtils.parseQuery(this.query);
+	var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
+	if(query.limit) {
+		limit = parseInt(query.limit, 10);
+	}
+	var minetype = null;
+	if(query.minetype)
+		minetype = query.minetype
+	if(limit < 0 || content.length < limit) {
+		return "module.exports = " + JSON.stringify("data:" + (minetype ? minetype + ";" : "") + "base64," + content.toString("base64"));
+	} else {
+		var fileLoader = require("file-loader");
+		return fileLoader.call(this, content);
 	}
 }
+module.exports.raw = true;
