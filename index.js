52ba8f0547a4bd3ca77685bdbf1da76aa4657e8c
Day <dayletter@gmail.com>
2014-03-18 19:26:49 +0800

    Fix limit = 0 to mean no limit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = function(content) {
 		mimetype = query.minetype
 	if(query.mimetype)
 		mimetype = query.mimetype
-	if(limit < 0 || content.length < limit) {
+	if(limit <= 0 || content.length < limit) {
 		return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
 	} else {
 		var fileLoader = require("file-loader");
