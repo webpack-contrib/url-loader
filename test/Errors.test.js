f9a7519fedab964422b5b9b6d8650966cf19b678
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2018-03-03 02:55:31 +0100

    test: standardize (`@webpack-contrib/test-utils`)

--- test/Errors.test.js
+++ test/Errors.test.js
@@ -1,11 +1,24 @@
 /* eslint-disable
   prefer-destructuring,
 */
-import loader from '../src';
+import webpack from '@webpack-contrib/test-utils';
 
 describe('Errors', () => {
-  test('Validation Error', () => {
-    const err = () => loader.call({ query: { limit: {} } });
+  test('Validation Error', async () => {
+    const config = {
+      loader: {
+        test: /\.png$/,
+        options: {
+          limit: {},
+        },
+      },
+    };
+
+    const stats = await webpack('fixture.js', config);
+    const { source } = stats.toJson().modules[0];
+
+    // eslint-disable-next-line
+    const err = () => eval(source);
 
     expect(err).toThrow();
     expect(err).toThrowErrorMatchingSnapshot();
